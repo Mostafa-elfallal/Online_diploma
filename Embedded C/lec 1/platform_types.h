87b46db087bf1813779773b8e746a28abe9e9c46
Mostafa Elfallal <mostafaelfallal90@gmail.com>
2021-02-13 01:50:18 +0200

    adding platform types

---
+++ Embedded C/lec 1/platform_types.h
@@ -0,0 +1,21 @@
+#ifndef __PLAT_H_
+#define __PLAT_H_
+typedef unsigned char 	boolen;
+
+typedef unsigned char 			uint8;
+typedef	signed char 			sint8;
+typedef unsigned short 			uint16;
+typedef	signed short 			sint16;
+typedef unsigned long 			uint32;
+typedef	signed long 			sint32;
+typedef volatile unsigned char 	vuint8;
+typedef	volatile signed char 	vsint8;
+typedef volatile unsigned short vuint16;
+typedef	volatile signed short 	vsint16;
+typedef volatile unsigned long 	vuint32;
+typedef	volatile signed long 	vsint32;
+
+typedef float 					float32;
+typedef double					float64;
+
+#endif
